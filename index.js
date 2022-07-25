78b3dadaef15c1249eebe3ec2dd2f5256f072444
Vlad Kel <108391008+dolsev@users.noreply.github.com>
2022-07-25 17:50:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-goFlip();
+console.log("before click");
+let cycles = 0;
+cycles++
+$("button").click(function(){goFlip()});
+
+console.log("after click");
 
 function goFlip (){
 
-    var randomNumber = Math.random();
+    let randomNumber = Math.random();
     if (randomNumber<0.5)
     {
         document.querySelector(".coin").setAttribute("src","images/heads.png");
         document.querySelector("h1").innerText="Heads!";
+        console.log ("heads");
     }
     else 
     {
         document.querySelector(".coin").setAttribute("src","images/tails.png");
         document.querySelector("h1").innerText="Tails!";
+        console.log("tails");
     }
     };
